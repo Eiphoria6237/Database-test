5d79cafb45af85c17da1a5876f6874878f1512ba
Eiphoria <liuerbsb@gmal.com>
2021-04-08 17:30:59 +0900

    Initial Commit

---
+++ FireBase1/FireBase1App.swift
@@ -0,0 +1,17 @@
+//
+//  FireBase1App.swift
+//  FireBase1
+//
+//  Created by LIU SHURUI on 2021/04/08.
+//
+
+import SwiftUI
+
+@main
+struct FireBase1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
