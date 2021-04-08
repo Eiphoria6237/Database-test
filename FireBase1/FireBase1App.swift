51fcd94597907365efb3a23202de631c58f44928
Eiphoria <liuerbsb@gmal.com>
2021-04-08 18:08:30 +0900

    add three pieces of data to firebase

--- FireBase1/FireBase1App.swift
+++ FireBase1/FireBase1App.swift
@@ -6,9 +6,29 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct FireBase1App: App {
+
+    init() {
+        FirebaseApp.configure()
+        makeReservation()
+    }
+    
+    func makeReservation() {
+        let db = Firestore.firestore()
+        let reservations = db.collection("reservations")
+        db.collection("shifts")
+        
+        reservations.document("test123").setData(["name":"Carla", "people":22])
+        
+        reservations.document().setData(["name":"Sue"])
+        
+        reservations.addDocument(data: ["name":"Tom", "people":10])
+        
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
